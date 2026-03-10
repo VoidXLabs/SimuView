7a7d7b9f728eec8726e506e105a3b7769ae52e4e
FuHao0119 <3144941746@qq.com>
2026-03-10 13:40:47 +0800

    初始化后端工程

---
+++ SimuViewBackEnd/src/main/java/com/voidxlab/simuview/SimuViewBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.voidxlab.simuview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuViewBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimuViewBackEndApplication.class, args);
+    }
+
+}
---
+++ SimuViewBackEnd/src/test/java/com/voidxlab/simuview/SimuViewBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.voidxlab.simuview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuViewBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
